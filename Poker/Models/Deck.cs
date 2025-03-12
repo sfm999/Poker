59b68bfa40a132339e9ccfbee4efca9ae0f1d92a
Stuart <stuart.miller@live.com.au>
2025-03-12 22:37:46 +1100

    Add Deck class. Implement Fisher-Yates shuffling algorithm

---
+++ Poker/Models/Deck.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker.Models
+{
+    public class Deck
+    {
+        Card[] cards;
+
+        public Deck()
+        {
+            cards = new Card[52];
+            PopulateCards();
+        }
+
+        public void PopulateCards()
+        {
+            Suit[] suits = { Suit.Spade, Suit.Club, Suit.Heart, Suit.Diamond };
+            Rank[] ranks =
+                    {
+                Rank.Ace  , Rank.Two  , Rank.Three,
+                Rank.Four , Rank.Five , Rank.Six,
+                Rank.Seven, Rank.Eight, Rank.Nine,
+                Rank.Ten  , Rank.Jack , Rank.Queen,
+                Rank.King
+            };
+            int index = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    cards[index] = new Card(suits[i], ranks[j]);
+                    index++;
+                }
+            }
+        }
+
+        public void DisplayAllCards()
+        {
+            foreach (Card c in cards)
+                Console.WriteLine($"{c.GetRank()}{c.GetSuit()}");
+        }
+
+        public void Shuffle()
+        {
+            var rand = new Random();
+            for (int i = cards.Length - 1; i > 1; i--)
+            {
+                int j = rand.Next(i);
+                Card temp = cards[i];
+                cards[i] = cards[j];
+                cards[j] = temp;
+            }
+        }
+    }
+}
--- Poker/Program.cs
+++ Poker/Program.cs
@@ -4,34 +4,12 @@ class Startup
 {
   static void Main(string[] args)
   {
-    // Welcome message
-    Console.WriteLine("Welcome to 5-hand Poker!");
+        // Welcome message
+        Console.WriteLine("Welcome to 5-hand Poker!");
 
-    Suit[] suits = { Suit.Spade, Suit.Club, Suit.Heart, Suit.Diamond };
-    Rank[] ranks = 
-            {
-                Rank.Ace  , Rank.Two  , Rank.Three,
-                Rank.Four , Rank.Five , Rank.Six,
-                Rank.Seven, Rank.Eight, Rank.Nine,
-                Rank.Ten  , Rank.Jack , Rank.Queen,
-                Rank.King
-            };
+        Deck deck = new Deck();
 
-    Card[] deck = new Card[52];
-    int index = 0;
-
-    for (int i = 0; i < 4; i++)
-    {
-      for (int j = 0; j < 13; j++)
-      {
-        deck[index] = new Card(suits[i], ranks[j]);
-        index++;
-      }
-    }
-
-    foreach (Card c in deck)
-    {
-      Console.WriteLine($"{c.GetRank()}{c.GetSuit()}");
-    }
+        deck.Shuffle();
+        deck.DisplayAllCards();
   }
 }
